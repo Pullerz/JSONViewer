ad7c7eb48f70fdacf59bf09acfc665485fe6e333
Alistair Pullen <herri98@gmail.com>
2025-09-01 13:13:22 +0100

    Initial Commit

---
+++ JSONViewer/JSONViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONViewerApp.swift
+//  JSONViewer
+//
+//  Created by Alistair Pullen on 01/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct JSONViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JSONViewerTests/JSONViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  JSONViewerTests.swift
+//  JSONViewerTests
+//
+//  Created by Alistair Pullen on 01/09/2025.
+//
+
+import Testing
+@testable import JSONViewer
+
+struct JSONViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
